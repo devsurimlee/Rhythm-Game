892a8625aeab2715ede96a6e313c411dfcf1c2c4
User <User@YD02-07>
2019-08-12 14:46:12 +0900

    패키지이름수정 + 라이브러리

---
+++ DynamicBeat/src/dynamic_beat_01/DynamicBeat.java
@@ -0,0 +1,15 @@
+package dynamic_beat_01;
+
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame {
+
+	public DynamicBeat() /*생성자*/{
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false); //한번 만들어진 창은 사용자가 임의로 줄이거나 늘릴 수 없다.
+		setLocationRelativeTo(null); //실행시 게임 화면이 정 중앙에 뜨게 설정
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //게임 창을 닫을때 프로그램 전체가 종료되는것
+		setVisible(true); //게임 화면이 정상적으로 출력되도록(보이게 하도록)
+	}
+}
---
+++ DynamicBeat/src/dynamic_beat_07/Main.java
@@ -0,0 +1,13 @@
+package dynamic_beat_07;
+
+public class Main {
+	
+	public static final int SCREEN_WIDTH = 1280;  //final은 한번 선언하면 절대 안바뀜, 상수는 전부 대문자
+	public static final int SCREEN_HEIGHT = 720;
+	
+	public static void main(String[] args) {
+		
+		new DynamicBeat();
+		
+	}
+}
